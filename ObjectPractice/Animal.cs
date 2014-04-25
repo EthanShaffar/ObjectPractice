7b3f8899c1e94539864a3c8027372c7f7b045717
SeedPaths-User <lessdismissive@email.com>
2014-04-25 14:47:47 -0600

    Initial
    Practice that i got very frustrated with

---
+++ ObjectPractice/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPractice
+{
+    class Animal
+    {
+        public string Name { get; set; }
+        public int Legs { get; set; }
+        public string TailLength { get; set; }
+
+        public void Animal(string name, int legs, string tailLength)
+        {
+            this.Name = name;
+            this.Legs = legs;
+            this.TailLength = tailLength;
+        }
+        
+    }
+}
---
+++ ObjectPractice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal snowMew = new Animal("Snow Leopard", "Feline", 4, "4 feet");
+            Animal arcticFox = new Animal("Arctic Fox", "Vulpine", 4, "2 feet");
+        }
+    }
+}
